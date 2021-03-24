0ea5a3a4966daf514effdc31284b2370202f7027
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-24 14:56:21 -0500

    create manage session server.js

---
+++ src/routes/auth/login.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+const API_URL = process.env.API_URL
+
+
+export const createHeaders = () => {
+  const headers = {
+    'accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+  return headers;
+}
+
+export const post = async (req, res) => {
+  try {
+    const headers = createHeaders();
+    const { username, password } = req.body;
+    const response = await axios.post(`${API_URL}/login`, {
+      username,
+      password
+    }, { headers });
+    req.session.user = {
+      token: response.token,
+      data: response.data,
+    };
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(req.session.user.data));
+  } catch (error) {
+    res.end(JSON.stringify({ error: error.message }));
+  }
+}
--- src/server.js
+++ src/server.js
@@ -3,18 +3,59 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 import { json } from 'body-parser';
-
+import session from 'express-session';
+import sessionFileStore from 'session-file-store';
+import helmet from 'helmet';
+import jwt from 'jsonwebtoken';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka()
-	.use(
-		json(),
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const FileStore = sessionFileStore(session);
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour milisegundos
+
+// TODO: implementar:
+// cookie: {
+//   secure: true,
+//   httpOnly: true,
+//   domain: 'example.com',
+//   path: 'foo/bar',
+//   expires: expiryDate
+// }
+
+polka() // You can also use Express
+  .use(
+    json(),
+    session({
+      secret: ['veryimportantsecret', 'notsoimportantsecret', 'highlyprobablysecret'],
+      name: 'sessionId',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: false,
+        maxAge: 31536000,
+        httpOnly: true,
+        expires: expiryDate
+      },
+      store: new FileStore({
+        path: process.env.NOW ? `/tmp/sapper/sessions` : `.sessions`
+      })
+    }))
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware({
+      session: req => (
+        {
+          user: req.session && req.session.user,
+          // TODO: si user existe sacar data
+          profile: req.session && req.session.user && jwt.decode(req.session.user.data.token)
+        })
+    })
+  )
+  .use(
+    helmet()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
