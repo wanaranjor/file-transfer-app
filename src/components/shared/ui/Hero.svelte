d2e704c31fbda01cc820c45e7bdfdda2f628b402
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-24 14:59:36 -0500

    creaete Hero component

---
+++ src/components/shared/ui/Hero.svelte
@@ -0,0 +1,18 @@
+<div
+  class="container flex flex-col items-center justify-center px-5 mx-auto text-gray-600"
+>
+  <img
+    class="object-cover object-center w-5/6 mb-10 rounded lg:w-2/6 md:w-3/6"
+    alt="hero"
+    src="upload.svg"
+  />
+  <div class="w-full text-center lg:w-2/3">
+    <h1 class="mb-4 text-3xl font-medium text-gray-900 sm:text-4xl">
+      Transferencia de Archivos
+    </h1>
+    <p class="mb-8 leading-relaxed">
+      Servicio para la transferencia de archivos. Permite archivos de texto,
+      imagenes, videos. El tamaño máximo permitido por archivo es de 200Mb.
+    </p>
+  </div>
+</div>
