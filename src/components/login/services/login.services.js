8494107c5de207bbd52499e1fdb315d060deec4c
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-24 14:59:21 -0500

    create Login component and service

---
+++ src/components/login/services/login.services.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+const API_URL = process.env.API_URL
+
+export const createHeaders = () => {
+  const headers = {
+    'accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+  return headers;
+}
+
+export const loginUser = async (username, password) => {
+  try {
+    const headers = createHeaders();
+    const url = "auth/login";
+    const response = await axios.post(url, {
+      username: username.trim(),
+      password: password,
+    }, { headers });
+    return response.data;
+  } catch (error) {
+    console.error('loginUser:', error);
+    return error;
+  }
+}
+
+export const logoutUser = async () => {
+  try {
+    const url = "auth/logout";
+    const response = await axios.post(url);
+    return response;
+  } catch (error) {
+    console.error('logoutUser:', error);
+    return error;
+  }
+}
+
+export const findUserById = async (id, token) => {
+  try {
+    const headers = createHeaders();
+    headers['Authorization'] = `Bearer ${token}`;
+    const url = `${API_URL}/users/${id}`;
+    const response = await axios.get(url, { headers });
+    return response;
+  } catch (error) {
+    console.error('findUserById:', error);
+    return error.response.status;
+  }
+}
---
+++ src/components/login/ui/Login.svelte
@@ -0,0 +1,95 @@
+<script>
+  import { loginUser } from "../services/login.services";
+  import { isEmpty, isValidEmail } from "./../../../helpers/validators";
+  import { NotificationDisplay, notifier } from "@beyonk/svelte-notifications";
+  import Hero from "../../shared/ui/Hero.svelte";
+  const titleApp = process.env.TITLE_APP;
+  let email,
+    password = "";
+
+  $: formIsValid = isValidEmail(email) && isEmpty(password);
+
+  // const handleSubmit = async () => {
+  //   const response = await loginUser(email, password);
+  //   if (!response.error) {
+  //     window.location.href = "/dashboard";
+  //   } else {
+  //     notifier.danger(
+  //       `Usuario o contraseña invalidos. Intente nuevamente.`,
+  //       5000
+  //     );
+  //     email = "";
+  //     password = "";
+  //   }
+  // };
+
+  const handleSubmit = async () => (window.location.href = "/dashboard");
+</script>
+
+<NotificationDisplay />
+<section class="flex flex-col items-center h-screen md:flex-row">
+  <div
+    class="hidden w-full h-screen bg-white md:items-center md:w-1/3 xl:w-2/3 md:flex"
+  >
+    <Hero />
+  </div>
+  <div
+    class="flex items-center justify-center w-full h-screen px-6 bg-blue-1300 md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 lg:px-16 xl:px-12"
+  >
+    <div class="w-full h-100">
+      <!-- <img src="logo.webp" alt="logo" /> -->
+      <h1
+        class="mt-12 text-2xl font-semibold tracking-tighter text-center text-gray-900 sm:text-3xl"
+      >
+        {titleApp}
+      </h1>
+      <form class="mt-6" on:submit|preventDefault={handleSubmit}>
+        <div>
+          <label
+            class="block font-medium leading-relaxed tracking-tighter text-gray-700 text-md"
+            for="email"
+            >Correo
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            bind:value={email}
+            class="w-full px-4 py-2 mt-2 text-base text-gray-700 bg-gray-100 border border-gray-300 focus:bg-white focus:ring-3 focus:outline-none focus:border-blue-700 focus:ring-blue-70"
+          />
+        </div>
+        <div class="mt-4">
+          <label
+            class="block font-medium leading-relaxed tracking-tighter text-gray-700 text-md"
+            for="password"
+            >Contraseña
+          </label>
+          <input
+            type="password"
+            name="password"
+            bind:value={password}
+            minlength="6"
+            class="w-full px-4 py-2 mt-2 text-base text-gray-700 bg-gray-100 border border-gray-300 focus:bg-white focus:ring-3 focus:outline-none focus:border-blue-700 focus:ring-blue-700"
+          />
+        </div>
+        <!-- TODO: pending create functionality -->
+        <!-- <div class="mt-2 text-right">
+          <a
+            href="/"
+            class="text-sm font-semibold leading-relaxed text-gray-700 hover:text-blue-700 focus:text-blue-700"
+            >¿Olvidó su Contraseña?</a
+          >
+        </div> -->
+        <button
+          class={formIsValid
+            ? "btn my-3"
+            : "btn opacity-50 cursor-not-allowed my-4"}
+          disabled={!formIsValid}
+          type="submit"
+          >Ingresar
+        </button>
+      </form>
+      <hr class="w-full my-6 border-gray-300" />
+    </div>
+  </div>
+</section>
