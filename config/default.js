e556d023e63ef9a2a8e0faab76ce7046a8189a37
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-24 15:01:27 -0500

    add TITLE_APP

--- config/default.js
+++ config/default.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 module.exports = {
   API_URL: process.env.API_URL,
-  URL_TELEM: process.env.URL_TELEM
+  URL_TELEM: process.env.URL_TELEM,
+  TITLE_APP: process.env.TITLE_APP
 }
