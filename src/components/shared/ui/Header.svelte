5917ab6f41a466408c1a51913b98aa660e45fa38
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-24 14:58:29 -0500

    create Header component

---
+++ src/components/shared/ui/Header.svelte
@@ -0,0 +1,49 @@
+<script>
+  const titleApp = process.env.TITLE_APP;
+</script>
+
+<header class="text-gray-600">
+  <div class="flex flex-col flex-wrap items-center p-5 md:flex-row">
+    <a
+      href="/"
+      class="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        class="w-10 h-10 p-2 text-white bg-indigo-500 rounded-full"
+        viewBox="0 0 24 24"
+      >
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+      </svg>
+      <span class="ml-3 text-xl font-bold">{titleApp}</span>
+    </a>
+    <nav
+      class="flex flex-wrap items-center justify-center text-base md:ml-auto"
+    >
+      <a href="/" class="mr-5 hover:text-gray-900">First Link</a>
+      <a href="/" class="mr-5 hover:text-gray-900">Second Link</a>
+      <a href="/" class="mr-5 hover:text-gray-900">Third Link</a>
+      <a href="/" class="mr-5 hover:text-gray-900">Fourth Link</a>
+    </nav>
+    <button
+      class="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0"
+      >Button
+      <svg
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        class="w-4 h-4 ml-1"
+        viewBox="0 0 24 24"
+      >
+        <path d="M5 12h14M12 5l7 7-7 7" />
+      </svg>
+    </button>
+  </div>
+</header>
