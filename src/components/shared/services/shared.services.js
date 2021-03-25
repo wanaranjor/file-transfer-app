403812e6f19bdd2026c704b3639b7445ae9fc04e
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-25 02:02:18 -0500

    create shared.services

---
+++ src/components/shared/services/shared.services.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+const API_URL = process.env.API_URL
+
+export const createHeaders = () => {
+  const headers = {
+    'accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+  return headers;
+}
+
+export const getInfoModel = async (model, token) => {
+  try {
+    const headers = createHeaders();
+    // headers['Authorization'] = `Bearer ${token}`;
+    const url = `${API_URL}/${model}`;
+    const { data } = await axios.get(url, { headers });
+    return (data.length > 0) ? data : [];
+  } catch (error) {
+    console.error(`${model}-> getModel:`, error);
+    return error;
+  }
+}
+
+
+// export const getUserById = async (userId, token) => {
+//   try {
+//     const headers = createHeaders();
+//     headers['Authorization'] = `Bearer ${token}`;
+//     const url = `${API_URL}/users/${userId}`;
+//     const { data } = await axios.get(url, { headers });
+//     return data;
+//   } catch (error) {
+//     console.error(`getUserById:`, error);
+//     return error;
+//   }
+// }
+
+// export const countInfoModelById = async (userId, model, token) => {
+//   try {
+//     const headers = createHeaders();
+//     headers['Authorization'] = `Bearer ${token}`;
+//     const url = `${API_URL}/${model}/count?filter={"where":{"userId":"${userId}"}}`;
+//     const { data } = await axios.get(url, { headers });
+//     return data.count;
+//   } catch (error) {
+//     console.error(`${model}-> countInfoModelById:`, error);
+//     return error;
+//   }
+// }
+
+
