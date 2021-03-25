7f0b95ffbf51522406e9e36f2cdb1f02c4a9ee75
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-25 02:01:58 -0500

    update default keys

--- config/default.js
+++ config/default.js
@@ -2,5 +2,6 @@ require('dotenv').config()
 module.exports = {
   API_URL: process.env.API_URL,
   URL_TELEM: process.env.URL_TELEM,
-  TITLE_APP: process.env.TITLE_APP
+  TITLE_APP: process.env.TITLE_APP,
+  MODEL_AREA: process.env.MODEL_AREA
 }
