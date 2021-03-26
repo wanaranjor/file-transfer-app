ef56205a1c3699d301544ccde90208444acb75cd
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-03-26 02:28:12 -0500

    update components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,31 +7,7 @@
 </script>
 
 <script>
-	import { stores } from "@sapper/app";
 	import Login from "../components/login/ui/Login.svelte";
-	const { session } = stores();
-	// console.log($session.user);
 </script>
-
-<!-- <script>
-	import Login from "../components/login/ui/Login.svelte";
-import FormUpload from "../components/Upload/ui/FormUpload.svelte";
-	import Hero from "../components/Upload/ui/Hero.svelte";
-	import ListFiles from "../components/Upload/ui/ListFiles.svelte";
-</script>
-
-<svelte:head>
-	<title>Transfer Files</title>
-</svelte:head>
-
-<section class="container flex flex-row items-center h-screen mx-auto">
-	<div class="w-full md:w-1/3">
-		<FormUpload />
-	</div>
-	<div class="md:w-2/3">
-		<Hero />
-		<ListFiles />
-	</div>
-</section> -->
 
 <Login />
